a028ef03c12c60790d57e372ae25efdf1223d2f2
funukonta <evanroy36@gmail.com>
2024-04-22 02:22:18 +0700

    feat  : add migration

--- pkg/database.go
+++ pkg/database.go
@@ -26,5 +26,57 @@ func ConnectPostgres() *sql.DB {
 		log.Panic("connection db not valid", err)
 	}
 
+	err = createTable(db)
+	if err != nil {
+		log.Panic("migration failed", err)
+	}
+
 	return db
+}
+
+func createTable(db *sql.DB) error {
+	query := `CREATE TABLE IF NOT EXISTS nationality (
+		nationality_id SERIAL PRIMARY KEY,
+		nationality_name VARCHAR(50) NOT NULL,
+		nationality_code CHAR(2) NOT NULL
+	);
+	
+	CREATE TABLE IF NOT EXISTS customer (
+		cst_id SERIAL PRIMARY KEY,
+		nationality_id INT NOT NULL,
+		cst_name CHAR(50) NOT NULL,
+		cst_dob DATE NOT NULL,
+		cst_phoneNum VARCHAR(20) NOT NULL,
+		cst_email VARCHAR(50) NOT NULL,
+		FOREIGN KEY (nationality_id) REFERENCES nationality(nationality_id)
+	);
+	
+	CREATE TABLE IF NOT EXISTS family_list (
+		fl_id SERIAL PRIMARY KEY,
+		cst_id INT NOT NULL,
+		fl_relation VARCHAR(50) NOT NULL,
+		fl_name VARCHAR(50) NOT NULL,
+		fl_dob DATE NOT NULL,
+		FOREIGN KEY (cst_id) REFERENCES customer(cst_id)
+	);
+	`
+	_, err := db.Exec(query)
+	if err != nil {
+		return err
+	}
+
+	query = `INSERT INTO nationality (nationality_name, nationality_code) VALUES
+	('United States', 'US'),
+	('United Kingdom', 'GB'),
+	('Canada', 'CA'),
+	('Australia', 'AU'),
+	('France', 'FR'),
+	('Indonesia', 'ID');`
+
+	_, err = db.Exec(query) // ignore error
+	if err != nil {
+		return nil
+	}
+
+	return nil
 }
