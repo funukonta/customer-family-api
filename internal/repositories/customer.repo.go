e6099bb69922e3ea9e4f68b726c261b7e1c9327e
funukonta <evanroy36@gmail.com>
2024-04-22 10:11:32 +0700

    fix : fix parameter in query

--- internal/repositories/customer.repo.go
+++ internal/repositories/customer.repo.go
@@ -29,7 +29,7 @@ func (r *customerRepo) Create(data *models.CustomerData) error {
 	}
 	defer tx.Rollback()
 
-	row := tx.QueryRow(`INSERT into customer (cst_name,cst_dob,cst_phoneNum,cst_email) values (?,?,?,?)
+	row := tx.QueryRow(`INSERT into customer (cst_name,cst_dob,cst_phoneNum,cst_email) values ($1,$2,$3,$4)
 	RETURNING cst_id`, data.Customer.Name, data.Customer.DOB, data.Customer.PhoneNum, data.Customer.Email)
 	err = row.Scan(&data.Customer.ID)
 	if err != nil {
@@ -37,8 +37,8 @@ func (r *customerRepo) Create(data *models.CustomerData) error {
 	}
 
 	for _, fam := range data.FamMember {
-		row := tx.QueryRow(`INSERT into family_list (cst_id,fl_relation,fl_name,fl_dob) values (?,?,?,?)
-		RETURNING fl_id`, fam.CustomerID, fam.Relation, fam.Name, fam.DOB)
+		row := tx.QueryRow(`INSERT into family_list (cst_id,fl_relation,fl_name,fl_dob) values ($1,$2,$3,$4)
+		RETURNING fl_id`, data.Customer.ID, fam.Relation, fam.Name, fam.DOB)
 		err := row.Scan(&fam.ID)
 		if err != nil {
 			return err
@@ -66,7 +66,7 @@ func (r *customerRepo) GetAllCustomer() ([]models.Customer, error) {
 }
 
 func (r *customerRepo) GetCustomer(id int) (*models.CustomerData, error) {
-	query := `SELECT cst_id,nationality_id,cst_name,cst_dob,cst_phoneNum,cst_email from customer where cst_id=?`
+	query := `SELECT cst_id,nationality_id,cst_name,cst_dob,cst_phoneNum,cst_email from customer where cst_id=$1`
 	row := r.DB.QueryRow(query, id)
 	cust := models.Customer{}
 	err := row.Scan(&cust.ID, &cust.NationalityID, &cust.Name, &cust.DOB, &cust.PhoneNum, &cust.Email)
@@ -74,7 +74,7 @@ func (r *customerRepo) GetCustomer(id int) (*models.CustomerData, error) {
 		return nil, err
 	}
 
-	query = `select fl_id,cst_id,fl_relation,fl_name,fl_dob from family_list where cst_id=?`
+	query = `select fl_id,cst_id,fl_relation,fl_name,fl_dob from family_list where cst_id=$1`
 	fm := []models.FamilyMember{}
 	rows, err := r.DB.Query(query, id)
 	if err != nil {
@@ -98,7 +98,7 @@ func (r *customerRepo) GetCustomer(id int) (*models.CustomerData, error) {
 }
 
 func (r *customerRepo) UpdateCustomer(data *models.CustomerData) error {
-	query := `UPDATE customer set cst_name=?,cst_dob=?,cst_phoneNum=?,cst_email=? where cst_id=?
+	query := `UPDATE customer set cst_name=$1,cst_dob=$2,cst_phoneNum=$3,cst_email=$4 where cst_id=$5
 	RETURNING cst_name,cst_dob,cst_phoneNum,cst_email`
 	tx, err := r.DB.BeginTx(context.Background(), nil)
 	if err != nil {
@@ -110,7 +110,7 @@ func (r *customerRepo) UpdateCustomer(data *models.CustomerData) error {
 		return err
 	}
 
-	query = `UPDATE family_list cst_id=?,fl_relation=?,fl_name=?,fl_dob=? where cst_id=?
+	query = `UPDATE family_list cst_id=$1,fl_relation=$2,fl_name=$3,fl_dob=$4 where cst_id=$5
 	RETURNING cst_id,fl_relation,fl_name,fl_dob`
 	for _, fam := range data.FamMember {
 		err = tx.QueryRow(query, fam.CustomerID, fam.Relation, fam.Name, fam.DOB).Scan(&fam.CustomerID, &fam.Relation, &fam.Name, &fam.DOB)
@@ -122,7 +122,7 @@ func (r *customerRepo) UpdateCustomer(data *models.CustomerData) error {
 }
 
 func (r *customerRepo) DeleteCustomer(data *models.DeleteCustomerReq, custID int) error {
-	query := `DELETE from customer where cst_id=?`
+	query := `DELETE from customer where cst_id=$1`
 	tx, err := r.DB.BeginTx(context.Background(), nil)
 	if err != nil {
 		return err
@@ -134,7 +134,7 @@ func (r *customerRepo) DeleteCustomer(data *models.DeleteCustomerReq, custID int
 		return err
 	}
 
-	query = `DELETE from family_list fl_id=?`
+	query = `DELETE from family_list fl_id=$1`
 	for _, fam := range data.FamId {
 		_, err = tx.Exec(query, fam.ID)
 		if err != nil {
